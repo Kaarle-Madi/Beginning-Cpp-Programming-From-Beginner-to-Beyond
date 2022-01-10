d218161acf576ebd504447563777c084413e141f
Kaarle Madi <76833027+KaarleMadi@users.noreply.github.com>
2022-01-10 19:18:54 +0700

    update

--- S7 Array and Vector/vector/vector/main.cpp
+++ S7 Array and Vector/vector/vector/main.cpp
@@ -9,7 +9,7 @@ int main()
 
 cout << "vector <int> vectorTest {1,2,3,4};" << endl;
 
-vector <int> vectorTest {1,2,3,4};
+    vector <int> vectorTest {1,2,3,4};
 
   /*  cout << "cout array" << endl;
     cout << vectorTest[0] << endl;
@@ -45,13 +45,16 @@ vector <int> vectorTest {1,2,3,4};
 
 
     cout << "-----------ADD TO THE VECTOR ---------------------------------" << endl;
-    int add_element {0};
+
+
+
     cout << "Update {0} place: ";
     cin >> vectorTest.at(0);
 
     cout << "Update {1} place: ";
     cin >> vectorTest.at(1);
 
+    int add_element {0};
     cout << "Enter to push_back: ";
     cin >> add_element;
     vectorTest.push_back(add_element);
@@ -67,7 +70,27 @@ vector <int> vectorTest {1,2,3,4};
     cout << "element in {3}: " << vectorTest.at(3) << endl;
     cout << "element in {4}: " << vectorTest.at(4) << " (This is push_back by cin)" << endl;
     cout << "element in {5}: " << vectorTest.at(5) << " (This is push_back by cin)" << endl;
-
+    cout << "size() There are " << vectorTest.size() << " elements" << endl;
 
     return 0;
 }
+
+
+/*
+
+#include <vector>
+using namespace std;
+
+vector<int> use_vector() {
+    //----WRITE YOUR CODE BELOW THIS LINE----
+
+    vector <int> vec {10,20,30,40,50};
+    vec.at(0) = 100;
+    vec.at(4) = 1000;
+
+    //----WRITE YOUR CODE ABOVE THIS LINE----
+    //----NO NOT MODIFY THE CODE BELOW THIS LINE----
+    return vec;
+}
+
+*/
