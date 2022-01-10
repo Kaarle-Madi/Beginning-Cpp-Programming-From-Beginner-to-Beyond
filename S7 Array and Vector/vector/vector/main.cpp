f907cf8a65906d5a643654f10c499d9839492aae
Kaarle Madi <76833027+KaarleMadi@users.noreply.github.com>
2022-01-10 18:23:37 +0700

    update

--- S7 Array and Vector/vector/vector/main.cpp
+++ S7 Array and Vector/vector/vector/main.cpp
@@ -11,33 +11,47 @@ int main()
 
 vector <int> vectorTest {1,2,3,4};
 
-    cout << "cout array" << endl;
+  /*  cout << "cout array" << endl;
     cout << vectorTest[0] << endl;
     cout << vectorTest[1] << endl;
     cout << vectorTest[2] << endl;
     cout << vectorTest[3] << endl;
-
-
-
-
+    */
     cout << "cout vector" << endl;
     cout << vectorTest.at(0) << endl;
+    cout << vectorTest.at(1) << endl;
+    cout << vectorTest.at(2) << endl;
+    cout << vectorTest.at(3) << endl;
+
+
     cout << "--------Update elements------------------------------" << endl;
     // vector
-    cin >> vectorTest.at(0); // cin change element in {0}
-    cin >> vectorTest.at(1); // cin change element in {1}
-    cout << "change element in {0}: " << vectorTest.at(0) << endl;
-    cout << "change element in {1}: " << vectorTest.at(1) << endl;
+    cin >> vectorTest.at(0); // cin update element in {0}
+    cin >> vectorTest.at(1); // cin update element in {1}
+    cout << "update element in {0}: " << vectorTest.at(0) << endl;
+    cout << "update element in {1}: " << vectorTest.at(1) << endl;
     cout << "-----------push_back---------------------------------" << endl;
 
-    vectorTest.push_back(321);
-    vectorTest.push_back(456);
+    vectorTest.push_back(0);
+
     cout << "element in {0}: " << vectorTest.at(0) << " (Updated element)" << endl;
     cout << "element in {1}: " << vectorTest.at(1) << " (Updated element)" << endl;
     cout << "element in {2}: " << vectorTest.at(2) << endl;
     cout << "element in {3}: " << vectorTest.at(3) << endl;
     cout << "element in {4}: " << vectorTest.at(4) << " (This is push_back)" << endl;
-    cout << "element in {5}: " << vectorTest.at(5) << " (This is push_back)" << endl;
+
+
+    cout << "-----------ADD TO THE VECTOR ---------------------------------" << endl;
+    int add_element {0};
+    cin >> add_element;
+    vectorTest.push_back(add_element);
+    cout << "element in {0}: " << vectorTest.at(0) << " (Updated element)" << endl;
+    cout << "element in {1}: " << vectorTest.at(1) << " (Updated element)" << endl;
+    cout << "element in {2}: " << vectorTest.at(2) << endl;
+    cout << "element in {3}: " << vectorTest.at(3) << endl;
+    cout << "element in {4}: " << vectorTest.at(4) << " (This is push_back)" << endl;
+    cout << "element in {5}: " << vectorTest.at(5) << " (This is push_back by cin)" << endl;
+
 
 
 
