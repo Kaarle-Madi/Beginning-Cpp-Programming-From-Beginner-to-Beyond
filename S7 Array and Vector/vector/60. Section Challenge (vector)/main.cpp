a7cb0513b7da0200b2e086e6fd99b2b216c4c993
Kaarle Madi <76833027+KaarleMadi@users.noreply.github.com>
2022-01-11 20:29:47 +0700

    update

--- S7 Array and Vector/vector/60. Section Challenge (vector)/main.cpp
+++ S7 Array and Vector/vector/60. Section Challenge (vector)/main.cpp
@@ -25,12 +25,16 @@ int main()
     cout << name_array[1] << endl;
     cout << name_array[2] << endl;
 
+    cout << "I'm array, this's my address in memeory:  " << name_array << endl;
+    // cout << "I've got total memers: " << name_array.size(); // only vector
+
     cout << "-----  Vector  elements -----" << endl;
     vector <int> name_vector {10,20,30};
     cout << name_vector.at(0) << endl;
     cout << name_vector.at(1) << endl;
     cout << name_vector.at(2) << endl;
 
+    cout << "I've got  " << name_vector.size() << " members" << endl;
     cout << "----- Update Vector element -----" << endl;
 /*  name_vector.at(0) = 100;
      name_vector.at(1) = 200;
